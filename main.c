60b04372931097859762a11c77fcaa9a6341a0c2
aleksandrabedynska <aleksandrabedynska95@gmail.com>
2018-03-14 17:14:56 +0100

    added comments

--- main.c
+++ main.c
@@ -3,29 +3,29 @@
 #include <limits.h>
 #include <stdlib.h>
 
-unsigned long long int factorial(int m);
-long double exp_squar(double a, int m);
+unsigned long long int factorial(int m);	// funcja liczaca silnie z liczby algorytmem rekurencyjnym, zwraca wartosc typu unsigned long long int aby liczyla silnie z mozliwie duzej liczby
+long double exp_squar(double a, int m);		//funkcja wykonujaca potegowanie liczby algorytmem szybkiego potegowania, zwraca wartosc typu long double
 
 void main()
 {
-	int n = 0;		// accuracy of the series
-	double x = 0.0f;	// argument for the function
-	long double error = 0.0f; 	
-	double ctgh_m = 0.0f;
-	long double ctgh = 0.0f;
-	long double numerator = 0.0f;
-	long double denominator = 0.0f;
-	double tgh = 0.0f;
+	int n = 0;			//dokladnosc rozwiniecia w szereg
+	double x = 0.0f;		//argument funkcji
+	long double error = 0.0f; 	// blad miedzy wartoscia funkcji obliczona z funkcji z math.h a wartoscia obliczona poprzez rozwiniecie w szereg
+	double ctgh_m = 0.0f;		// zmienna przechowujaca wartosc funkcji kotangens hiperboliczny obliczona za pomoca odwrotnosci funkcji tangens hiperboliczny zawartej w bibliotece math.h
+	long double ctgh = 0.0f;	// zmienna przechowaujaca wartosc funkcji kotangens hiperboliczny obliczonej przez rozwiniecie w szereg
+	long double numerator = 0.0f;		// licznik 
+	long double denominator = 0.0f;		//mianownik
+	double tgh = 0.0f;		//zmienna pomocnicza podajaca wartosc tangensa hiperbolicznego obliczonego korzystajac z funkcji z biblioteki math.h
 
 	printf("Type the argument for hyperbolic cotangens function (x) \n");
 	scanf("%lf", &x);
 	printf("Type the desired accuracy of series (n) \n");
 	scanf("%d", &n);
-	if (n > 0 && round(n) == n)
+	if (n > 0 && round(n) == n)		//petla realizujaca program jesli podana przez uzytkownika funkcja n jest liczba dodatnia i calkowita
 	{
 		tgh= tanh(x);
 		ctgh_m = 1. / tgh;
-		printf("tanh = %lf \n",tgh);
+		//printf("tanh = %lf \n",tgh);
 		for (int i = 0; i < n + 1; i++)
 		{
 			numerator = numerator + exp_squar(x, 2 * i) / factorial(2 * i);
